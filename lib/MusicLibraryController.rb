0839ebcb37dbbf8e62437548e34d6f77576bcb4b
Jos Castro <joselycas@gmail.com>
2018-01-24 18:35:14 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -76,6 +76,6 @@ attr_accessor :path
   def play_song
     puts "Which song number would you like to play?"
     index = gets.strip.to_i-1
-    song = Song.sorted_songs
+    song = Song.sorted_songs[0]
   end
 end
